71ccee9757381330c1fe40566f9755f056069fae
macbookproretina <se-sercan@outlook.com>
2018-05-04 18:46:37 +0300

    My Recipe List some components are added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import {LoginPage} from "../pages/login/login";
 import {SignupPage} from "../pages/signup/signup";
+import {RecipeListPage} from "../pages/recipe-list/recipe-list";
 @Component({
   templateUrl: 'app.html'
 })
